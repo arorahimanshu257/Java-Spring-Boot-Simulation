97390cfb5104bb39e8a858dd6741f58b80a7caa4
Himanshu Arora <38257545+arorahimanshu257@users.noreply.github.com>
2025-06-24 15:58:02 +0530

    Initial upload of Spring Boot Blog App project

---
+++ src/main/java/com/example/milestonemanager/MilestoneManagerApplication.java
@@ -0,0 +1,11 @@
+package com.example.milestonemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilestoneManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MilestoneManagerApplication.class, args);
+    }
+}
